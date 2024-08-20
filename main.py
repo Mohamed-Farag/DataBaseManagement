ac1b8f3d499d56c6a72130dcf5917f7260c9a282
Mohamed Farag <33036643+Mohamed-Farag@users.noreply.github.com>
2024-08-20 18:46:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from utils import databaseManager
+from utils import report
+import os
+import setupEnvironment
+
+def main():
+
+    # Get the configuration and sample data
+    config = setupEnvironment.setup_sample_environment()
+
+    # Access and get the values
+    db_path = config['db_path']
+    CSV_file_path = config['CSV_file_path']
+    json_file_path = config['json_file_path']
+    report_file_path = config['report_file_path']
+    sample_records = config['sample_records']
+
+    # connect to database
+    db_manager = databaseManager.DatabaseManager(db_path)
+
+    # Create the table
+    db_manager.create_table()
+
+    # Insert data
+    db_manager.insert_data(sample_records)
+
+    # Export data to CSV to output folder (data.csv)
+    db_manager.export_to_csv(CSV_file_path)
+
+    # Export data to JSON to output folder (data.json)
+    db_manager.export_to_json(json_file_path)
+
+    # Close the connection
+    db_manager.close_connection()
+
+    # Generate report to output folder (report.pdf)
+    report_generator = report.ReportGenerator(CSV_file_path, json_file_path, report_file_path)
+    report_generator.generate_report()
+
+if __name__ == '__main__':
+    main()
---
+++ setupEnvironment.py
@@ -0,0 +1,54 @@
+import os
+
+def setup_sample_environment():
+    # name of the files
+    db_name = "sample.db"
+    CSV_file_name = "data.csv"
+    json_file_name = "data.json"
+    report_file_name = "report.pdf"
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Add output folder to exporting result
+    output_dir = os.path.join(script_dir, "output")
+
+    # Create the output directory if it does not exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Build the path to the SQLite database, JSON and CSV files in the same directory as the script
+    db_path = os.path.join(output_dir,db_name)
+    CSV_file_path = os.path.join(output_dir,CSV_file_name)
+    json_file_path = os.path.join(output_dir,json_file_name)
+    report_file_path = os.path.join(output_dir,report_file_name)
+
+    # Sample data to be inserted (10 records)
+    '''template:
+     - id (INTEGER) --> primary key, auto increment 
+     - name (TEXT)
+     - email (TEXT)
+     - zip_code (TEXT)
+     - title (TEXT)
+    '''
+    # create sample of records
+    sample_records = [
+        ('Mohamed Emad', 'Mohamed@siemens.com', '10001', 'Engineer'),
+        ('Mostafa Khaled', 'Mostafa@valeo.com', '10002', 'Manager'),
+        ('Abeer Ali zein', 'Abeer@eJad.com', '10003', 'Technician'),
+        ('Khaled Morsy nour', 'Khaled@vodafone.com', '10004', 'Engineer'),
+        ('Lotfy Elzein', 'Lotfy@orange.com', '10005', 'Analyst'),
+        ('Diana Shaker', 'Diana@siemens.com', '10006', 'Director'),
+        ('George', 'george@valeo.com', '10007', 'Consultant'),
+        ('Hannah Lee', 'hannah@valeo.com', '10008', 'Director'),
+        ('Ian Clarke', 'ian@eJad.com', '10009', 'Manager'),
+        ('Jasmine Yasser', 'jasmine@siemens.com', '10010', 'Assistant')
+    ]
+
+    return {
+        'db_path': db_path,
+        'CSV_file_path': CSV_file_path,
+        'json_file_path': json_file_path,
+        'report_file_path': report_file_path,
+        'sample_records': sample_records
+    }
